b51bbebc7fd67473f32c564232019c5e7b9b2b0d
Julien.merveaux <jaimet@outlook.fr>
2024-01-24 11:40:21 +0100

    pop up well

--- src/components/PopUpDescriptionVue.vue
+++ src/components/PopUpDescriptionVue.vue
@@ -2,7 +2,17 @@
   <div>
     <div class="popup" :class="{ open: isOpen }">
       <div class="popup_content">
-        <h1>test</h1>
+        <div class="popup_general">
+          <div class="popup_gauche">
+            <img class="photoGauche" src=../assets/Ferme%20florale%20les%205%20saisons%201.jpg>
+          </div>
+          <div class="popup_droite">
+            <H1>PHLOX PANICULATA PAX</H1>
+            <H2>Plante vivace</H2>
+            <H3>3,75€</H3>
+            <h4>dazfdefzefceafeaf fezfze f fze fzef zef zefze fef  f zf zf fz fzfzfzfzefzefze f ze fzfzf zfz  fz fz</h4>
+          </div>
+        </div>
         <button class="popup-exit" @click="closePopup">Fermer</button>
       </div>
     </div>
@@ -21,6 +31,33 @@ const closePopup = () => {
 </script>
 
 <style scoped>
+
+.photoGauche {
+  width: 100%;
+  aspect-ratio: 1;
+}
+
+.popup_gauche {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 80%;
+}
+.popup_droite {
+  text-align: center;
+  width: 50%;
+  height: 80%;
+}
+.popup_general {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  gap: 20px;
+}
+
 .popup {
   display: none;
   background-color: rgba(0, 0, 0, 0.5);
@@ -29,21 +66,35 @@ const closePopup = () => {
   left: 0;
   right: 0;
   bottom: 0;
-  z-index: 1;
+  z-index:10;
   display: flex;
   justify-content: center;
   align-items: center;
+  box-shadow: #090909;
 }
 
 .popup_content {
+  display: flex;
+  flex-direction: column;
   background-color: white;
-  max-width: 600px;
+  width: 50%;
+  height: auto;
   padding: 30px;
   box-sizing: border-box;
   position: relative;
+  gap: 10px;
 }
 
 .popup.open {
   display: flex;
+}
+
+.popup-exit {
+  background-color: transparent;
+  border: none;
+  bottom: 100%;
+  left: 48%;
+  position: relative;
+
 }
 </style>
