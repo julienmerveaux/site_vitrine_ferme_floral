09557b713858a1eb3b19eb69f4d6803a90c233d0
Julien.merveaux <jaimet@outlook.fr>
2024-01-26 16:54:31 +0100

    its good

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/site_vitrine_ferme_floral/', // Assurez-vous que ce chemin est correct
+  mimeTypes: {
+    '.js': 'module/javascript',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
